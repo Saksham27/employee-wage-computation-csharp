7a327824e171af986619bae8136f19395ccdddd3
Saksham27 <geekysaksham27@gmail.com>
2020-03-31 12:37:43 +0530

    Added: created project named empWageComputation

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace empWageComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
