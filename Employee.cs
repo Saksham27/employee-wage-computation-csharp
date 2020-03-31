66c0ad1e65ba0425bb98a63384e48b2295b1ebc7
Saksham27 <geekysaksham27@gmail.com>
2020-03-31 14:57:17 +0530

    Added: method to calculate full time employee wage for a day

--- Employee.cs
+++ Employee.cs
@@ -8,10 +8,17 @@ namespace empWageComputation
     {
         private int PRESENT = 0;
 
+        // Method to check if employee is present or not
         public bool employeeAttendance()
         {
             Random r = new Random();
-            return r.Next(0, 1) == 0 ? true : false;
+            return r.Next(0, 2) == PRESENT ? true : false;
+        }
+
+        // Method to calculate employee wage for 1 day
+        public int dailyEmployeeWage(int wage, int workingHours)
+        {
+            return wage * workingHours;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,12 +4,30 @@ namespace empWageComputation
 {
     class Program
     {
+        private static int WAGE_PER_HOUR = 20;
+        private static int FULL_DAY_HOUR = 8;
+        private static int NO_WAGE = 0;
+
+        private static bool isEmployeePresent;
+        private static int wageForADay;
+
         static void Main(string[] args)
         {
-            bool isEmployeePresent;
+
             Console.WriteLine("*** Welcome to Employee Wage Computation ***");
             Employee emp = new Employee();
             isEmployeePresent = emp.employeeAttendance();
+            
+            if ( isEmployeePresent == true )
+            {
+                wageForADay = emp.dailyEmployeeWage(WAGE_PER_HOUR, FULL_DAY_HOUR);
+            }
+            else
+            {
+                wageForADay = NO_WAGE;
+            }
+
+            Console.WriteLine( $"{isEmployeePresent}, {wageForADay}");
         }
     }
 }
