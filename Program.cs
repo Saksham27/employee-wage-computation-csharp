cd7274206a14ef2f8ed618e87c81b4642c19c57c
Saksham27 <geekysaksham27@gmail.com>
2020-03-31 12:42:38 +0530

    Added; A welcome message

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace empWageComputation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("*** Welcome to Employee Wage Computation ***");
         }
     }
 }
